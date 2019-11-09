1b868a97d475dcc0fdf9577e0b399231644da1a1
Rex Isaac Raphael <rex.raphael@outlook.com>
2019-11-09 20:10:39 +0100

    support for typegql

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,3 @@
-// @TOD0: add support for code-first approach
-
 export * from './nestjs-graphql-distributed-gateway.module';
 export * from './nestjs-graphql-distributed.module';
 export * from './decorators';
